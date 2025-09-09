a33a0720339d40e5f236b418d7dcb6c882255c8a
Jet Pham <jetthomaspham@gmail.com>
2025-09-09 15:43:13 -0700

    feat: add noisebridge link to image

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,16 @@ import Noisebridge from "./assets/noisebridge_logo.utf8ans";
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <Ansi>{Noisebridge}</Ansi>
+      <a
+        href="https://noisebridge.net"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="outline-none focus:outline-white"
+        tabIndex={0}
+        aria-label="Visit Noisebridge.net"
+      >
+        <Ansi>{Noisebridge}</Ansi>
+      </a>
     </div>
   );
 }
