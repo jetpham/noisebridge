e6907ce4c16155c1504c0d2b3b58e5c8633656d8
Jet Pham <jetthomaspham@gmail.com>
2025-09-09 15:34:54 -0700

    feat: center the ansi onto the page

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,9 @@ import Ansi from "./components/ansi";
 import Noisebridge from "./assets/noisebridge_logo.utf8ans";
 
 export default function Home() {
-  return <Ansi>{Noisebridge}</Ansi>;
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Ansi>{Noisebridge}</Ansi>
+    </div>
+  );
 }
