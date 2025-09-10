5a10247084cfde32e07af58197e4260f6975d9b6
Jet Pham <jetthomaspham@gmail.com>
2025-09-09 18:20:04 -0700

    feat: add image previews

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,30 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NODE_ENV === 'production' ? 'https://noisebridge.net' : 'http://localhost:3000'),
   title: "Noisebridge",
   description: "Noisebridge",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+  openGraph: {
+    images: [
+      {
+        url: "<generated>",
+        type: "<generated>",
+        width: "<generated>",
+        height: "<generated>",
+      },
+    ],
+  },
+  twitter: {
+    images: [
+      {
+        url: "<generated>",
+        type: "<generated>",
+        width: "<generated>",
+        height: "<generated>",
+      },
+    ],
+  },
   other: {
     "apple-mobile-web-app-title": "Noisebridge",
   },
